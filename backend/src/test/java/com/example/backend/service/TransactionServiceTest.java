a048335619acbe625768cf8a99df5a4be85e07f5
hinajawaid <hinajawaid.h@gmail.com>
2025-06-15 18:47:43 +0200

    Unit Test updated with newly added field - for maven CI check

--- backend/src/test/java/com/example/backend/service/TransactionServiceTest.java
+++ backend/src/test/java/com/example/backend/service/TransactionServiceTest.java
@@ -31,7 +31,8 @@ class TransactionServiceTest {
 
     private User user;
     private Account account;
-    private TransactionDto transactionDto;
+    private TransactionDto creditTransactionDto;
+    private TransactionDto debitTransactionDto;
 
     @BeforeEach
     void setUp() {
@@ -45,7 +46,7 @@ class TransactionServiceTest {
         account.setAccountBalance(1000.0);
         account.setUser(user);
 
-        transactionDto = new TransactionDto(
+        creditTransactionDto = new TransactionDto(
                 user.getId(),
                 account.getId(),
                 null,
@@ -58,7 +59,7 @@ class TransactionServiceTest {
                 TransactionStatus.SUCCESS,
                 TransactionDirection.CREDIT
         );
-        transactionDto = new TransactionDto(
+        debitTransactionDto = new TransactionDto(
                 user.getId(),
                 account.getId(),
                 null,
@@ -77,7 +78,7 @@ class TransactionServiceTest {
     void addMoneyTransaction_shouldReturnSuccessResponse_whenAccountExists() {
         when(mockAccountRepo.findById(account.getId())).thenReturn(Optional.of(account)); //mocked, return account when searched by Id
         when(mockUserRepo.getById(user.getId())).thenReturn(user);//mocked, return user when searched by Id
-        TransactionResponse response = transactionService.addTransaction(transactionDto); //call service
+        TransactionResponse response = transactionService.addTransaction(creditTransactionDto); //call service
         assertNotNull(response); //response not null
         assertEquals("Transaction Successful",response.message());
         assertEquals(TransactionStatus.SUCCESS.name(),response.transactionStatus());
@@ -90,7 +91,7 @@ class TransactionServiceTest {
     @Test
     void addMoneyTransaction_shouldReturnFailedResponse_whenAccountNotFound() {
         when(mockAccountRepo.findById(account.getId())).thenReturn(Optional.empty()); //return account empty
-        TransactionResponse response = transactionService.addTransaction(transactionDto); //call service
+        TransactionResponse response = transactionService.addTransaction(creditTransactionDto); //call service
         assertNotNull(response);
         assertEquals("Account not found",response.message());
         assertEquals(TransactionStatus.FAILED.name(),response.transactionStatus());
@@ -102,7 +103,7 @@ class TransactionServiceTest {
     void transferTransaction_shouldReturnSuccessResponse_whenAccountExists_andSufficientBalance() {
         when(mockAccountRepo.findById(account.getId())).thenReturn(Optional.of(account)); //return account
         when(mockUserRepo.getById(user.getId())).thenReturn(user); //return user
-        TransactionResponse response = transactionService.transferTransaction(transactionDto); //call service
+        TransactionResponse response = transactionService.transferTransaction(debitTransactionDto); //call service
         assertNotNull(response); //response not null
         assertEquals("Transaction Successful",response.message()); //transaction successful
         assertEquals(TransactionStatus.SUCCESS.name(),response.transactionStatus()); //status success
@@ -114,7 +115,7 @@ class TransactionServiceTest {
     @Test
     void transferTransaction_shouldReturnFailedResponse_whenAccountNotFound_andSufficientBalance() {
         when(mockAccountRepo.findById(account.getId())).thenReturn(Optional.empty());//account empty
-        TransactionResponse response = transactionService.transferTransaction(transactionDto); //call service
+        TransactionResponse response = transactionService.transferTransaction(debitTransactionDto); //call service
         assertNotNull(response); //response not null
         assertEquals("Account not found",response.message());
         assertEquals(TransactionStatus.FAILED.name(),response.transactionStatus());
@@ -124,7 +125,7 @@ class TransactionServiceTest {
     @Test
     void transferTransaction_shouldReturnFailedResponse_whenInsufficientBalance() {
         account.setAccountBalance(100.00);
-        transactionDto = new TransactionDto(
+        debitTransactionDto = new TransactionDto(
                 user.getId(), account.getId(), null,  100.00, 200.00, "Test deposit with insufficient balance",
                 "bankTransfer", "Account Holder: Tester, IBAN: DE977898992789, BIC: BIC6872",
                 new Date(), TransactionStatus.FAILED,
@@ -133,7 +134,7 @@ class TransactionServiceTest {
         when(mockAccountRepo.findById(account.getId())).thenReturn(Optional.of(account));
         when(mockUserRepo.getById(user.getId())).thenReturn(user);
 
-        TransactionResponse response = transactionService.transferTransaction(transactionDto);
+        TransactionResponse response = transactionService.transferTransaction(debitTransactionDto);
         assertNotNull(response);
         assertEquals("Transaction Failed: Account Balance low to transfer requested amount",response.message());
         assertEquals(TransactionStatus.FAILED.name(),response.transactionStatus());
