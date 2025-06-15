ebded0c7158ca399dd23342abe4bd9a7142992bf
hinajawaid <hinajawaid.h@gmail.com>
2025-06-15 18:32:46 +0200

    transaction dto reverted

--- backend/src/main/java/com/example/backend/model/TransactionDto.java
+++ backend/src/main/java/com/example/backend/model/TransactionDto.java
@@ -6,16 +6,16 @@ import com.example.backend.enums.TransactionStatus;
 import java.util.Date;
 
 public record TransactionDto(
+        Long userId,
+        Long accountId,
         Long id,
-        Long fromAccountId,
-        Long toAccountId,
-        double amount,
-        Double fee,
-        String currency,
-        String description,
-        String reference,
-        Date date,
+        double accountBalance, //this should be sent from account table later - to view current account
+        Double amount,
+        String description, //example amount added in account or amount transferred from account
+        String transactionType, //added or transferred
+        String transactionFromToAccountDetails, //transferred from acc/card: acc card detail or transferred to
+        Date transactionDate,
         TransactionStatus status,
-        TransactionDirection direction
+        TransactionDirection transactionDirection
 ) {
 }
